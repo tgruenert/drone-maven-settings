90ed1f1a30d0daf8d968ae5d23035003ba7e74d8
Sven Linstaedt <sven.linstaedt@gmail.com>
2020-05-12 18:07:40 +0200

    fix: use correct env variable to detect ci build

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,7 +8,7 @@ function isenv {
 isenv LOCAL_CACHE || export LOCAL_CACHE='${user.home}/.m2/repository'
 isenv OUTPUT || test -d .mvn || mkdir .mvn
 isenv OUTPUT || OUTPUT=".mvn/release-settings.xml" && echo " -s $OUTPUT" >> .mvn/maven.config
-isenv CI_WORKSPACE && echo "Rendering $OUTPUT" || OUTPUT="/dev/stdout"
+isenv DRONE_WORKSPACE && echo "Rendering $OUTPUT" || OUTPUT="/dev/stdout"
 
 export PROPERTIES=$(env2args dot '<$k>$v</$k>' PLUGIN_PROPERTY_)
 for _env in $(env2args); do export $_env; done
